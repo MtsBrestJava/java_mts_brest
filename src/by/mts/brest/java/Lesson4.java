abb78594922d0aaaf714e13f47eef4340a09b736
sergeblr2 <sergeblr2@gmail.com>
2020-09-26 13:09:03 +0300

    Massiv, initialization, part2

--- src/by/mts/brest/java/Lesson4.java
+++ src/by/mts/brest/java/Lesson4.java
@@ -1,20 +1,27 @@
 package by.mts.brest.java;
 
+import java.util.Arrays;
+
 public class Lesson4 {
     public static void main(String[] args) {
 
         int nash_massive0[];
         nash_massive0 = new int[3];
+        System.out.println("nash_massive0: "+ Arrays.toString(nash_massive0));
 
         int nash_massive1[] = new int[5];     // int[] nash_massive;
+        System.out.println("nash_massive1: "+ Arrays.toString(nash_massive1));
 
         int nash_massive2[] = new int[] {1, 22, 11, 4, 5435, 6, 7};
+        System.out.println("nash_massive2: "+ Arrays.toString(nash_massive2));
 
         int nash_massive3[] = new int[4];
         nash_massive3[0] = 98;
         nash_massive3[1] = 34;
         nash_massive3[2] = 1;
         nash_massive3[3] = 1928;
+        System.out.println("nash_massive3: "+ Arrays.toString(nash_massive3));
+
 
 
     }
