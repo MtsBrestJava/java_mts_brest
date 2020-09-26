fe80a5d56d502143063689db6c98593a3faf7bfb
sergeblr2 <sergeblr2@gmail.com>
2020-09-26 13:02:17 +0300

    Massiv, initialization

---
+++ src/by/mts/brest/java/Lesson4.java
@@ -0,0 +1,21 @@
+package by.mts.brest.java;
+
+public class Lesson4 {
+    public static void main(String[] args) {
+
+        int nash_massive0[];
+        nash_massive0 = new int[3];
+
+        int nash_massive1[] = new int[5];     // int[] nash_massive;
+
+        int nash_massive2[] = new int[] {1, 22, 11, 4, 5435, 6, 7};
+
+        int nash_massive3[] = new int[4];
+        nash_massive3[0] = 98;
+        nash_massive3[1] = 34;
+        nash_massive3[2] = 1;
+        nash_massive3[3] = 1928;
+
+
+    }
+}
