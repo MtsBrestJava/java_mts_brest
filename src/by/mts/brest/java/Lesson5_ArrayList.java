dc695ababd3d69a2afa62b1d2591fb59cdf54829
sergeblr2 <sergeblr2@gmail.com>
2020-10-03 12:21:45 +0300

    Lesson5 - ArrayList3

--- src/by/mts/brest/java/Lesson5_ArrayList.java
+++ src/by/mts/brest/java/Lesson5_ArrayList.java
@@ -5,13 +5,19 @@ import java.util.ArrayList;
 public class Lesson5_ArrayList {
     public static void main(String[] args) {
 
+        // Список<типДанных> ИмяСписка = new Список<типДанных>()
         ArrayList<Integer> someInts = new ArrayList<Integer>();
+        ArrayList<ArrayList<Integer>> someInts2 = new ArrayList<ArrayList<Integer>>();
+
         someInts.add(55);
         someInts.add(12);
         someInts.add(99);
-        someInts.add(3, 222);
+        System.out.println("someInts = " + someInts);
+        someInts.add(1, 222);
         System.out.println("someInts = " + someInts);
         someInts.set(1, 909);
+        System.out.println("someInts = " + someInts);
+        someInts.remove(1);
         System.out.println("someInts = " + someInts);
 
     }
