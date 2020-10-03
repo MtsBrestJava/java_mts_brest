88674a5863a927ce9b82fb30f518916c11692958
sergeblr2 <sergeblr2@gmail.com>
2020-10-03 12:51:07 +0300

    Lesson5 - ArrayList6

--- src/by/mts/brest/java/Lesson5_ArrayList.java
+++ src/by/mts/brest/java/Lesson5_ArrayList.java
@@ -8,10 +8,16 @@ public class Lesson5_ArrayList {
         // Список<типДанных> ИмяСписка = new Список<типДанных>()
         ArrayList<Integer> someInts = new ArrayList<Integer>(); //1мерн
         ArrayList<ArrayList<Integer>> someInts2 = new ArrayList<ArrayList<Integer>>(); //2мерн
+        ArrayList<Integer> someInts3 = new ArrayList<Integer>(); //1мерн
+        someInts3.add(999);
+        someInts3.add(888);
+        someInts3.add(777);
 
         someInts.add(55);
         someInts.add(12);
         someInts.add(99);
+        someInts.add(null);
+        someInts.add(0);
         System.out.println("someInts = " + someInts);
         someInts.add(1, 222);
         System.out.println("someInts = " + someInts);
@@ -19,7 +25,11 @@ public class Lesson5_ArrayList {
         System.out.println("someInts = " + someInts);
         someInts.remove(1);
         System.out.println("someInts = " + someInts);
-        
+        System.out.println("someIntsINDEXED[3] = " + someInts.get(2)); // получает 2-й элемент из списка
+        System.out.println("someInts = " + someInts.indexOf(99)); // ищет первое совпадение числа в списке (возвращает индекс в массиве)
+        System.out.println("someInts = " + someInts.indexOf(999)); // не находит число, возвращает -1
+        System.out.println("someInts = " + someInts.size()); // не находит число, возвращает -1
+
 
     }
 }
