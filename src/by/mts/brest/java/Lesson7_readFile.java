b329d19620960e218f90ba433c9bd9b94bf466e3
sergeblr2 <sergeblr2@gmail.com>
2020-10-10 13:38:32 +0300

    FileReader and FileWriter3

--- src/by/mts/brest/java/Lesson7_readFile.java
+++ src/by/mts/brest/java/Lesson7_readFile.java
@@ -12,12 +12,12 @@ public class Lesson7_readFile {
 
             System.out.println("stroka1 = " + reader.readLine());   // Выводим первую строчку с файла в консоль
             System.out.println("stroka2 = " + reader.readLine());   // ... вторую
-            System.out.println("stroka3 = " + (char)reader.read());   // ... третюю???
+            System.out.println("stroka3 = " + (char)reader.read());   // ... ТОЛЬКО первый символ из третей строки
             // ... for()? while()?
         reader.close();     // Закрываем файл
 
         // Создаем файл
-        FileWriter writer = new FileWriter("C:\\my\\file2.txt");
+        FileWriter writer = new FileWriter("C:\\my\\file2.txt",true);
         writer.write("aaaaaa\n");   // Записываем в файл строки
         writer.write("333333");
         writer.close();     // Закрываем файл
