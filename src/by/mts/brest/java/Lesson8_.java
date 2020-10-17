9cade4bd2c183c1295a0015253d6b21d27fb2260
sergeblr2 <sergeblr2@gmail.com>
2020-10-17 13:25:25 +0300

    Lesson7 + full2

--- src/by/mts/brest/java/Lesson8_.java
+++ src/by/mts/brest/java/Lesson8_.java
@@ -6,17 +6,39 @@ public class Lesson8_ {
     public static void main(String[] args) {
         int x = 10;
         int y = 20;
-
         int z;
         z = y + 1;
         System.out.println("z = " + z);
 
+        // Vvod dannyh ili chtenie fajla
+        // *****
+
+        // !!!! Obrabotka i itog #1
+/*        int poditog, itog;
+        poditog = (z * x + y)*22;
+        System.out.print("poditog = " + poditog + "; ");
+        itog = poditog*64 - y%2;
+        System.out.println("itog = " + itog);*/
+
+        // !!!! Obrabotka i itog #2
+        // --/--
+
+        // !!!! Obrabotka i itog #3 с другими данными
+        // --/--
+
+    }
+
+    int calculateItog(int z, int x, int y) {
         int poditog, itog;
         poditog = (z * x + y)*22;
         System.out.print("poditog = " + poditog + "; ");
         itog = poditog*64 - y%2;
         System.out.println("itog = " + itog);
     }
+
+    void
+
+
 
 
 }
