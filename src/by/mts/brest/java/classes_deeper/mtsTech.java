e1d573f42f3b14a6ac317ab421e56b1eb44d0643
sergeblr <sergeblr@mail.ru>
2020-11-20 13:48:39 +0300

    Human + mtsTech + mtsSale - examples of inheritance and Main BASIC

--- src/by/mts/brest/java/classes_deeper/mtsTech.java
+++ src/by/mts/brest/java/classes_deeper/mtsTech.java
@@ -1,10 +1,28 @@
 package by.mts.brest.java.classes_deeper;
 
 public class mtsTech extends Human {
+    // Класс mtsTech наследуется от Human со всеми полями и методами КРОМЕ конструкторов
 
+    // Свои поля для класса mtsTech (кроме тех что он унаследовал)
+    String jobSpecialization;        // Специализация работы, должность
+    int driveLevel;                  // Уровень пользования служебным транспортом
+
+    // Поэтому объявляем здесь конструктор с двумя полыми String
     mtsTech(String name, String surname) {
-        super(name, surname);
+        super(name, surname);           // Т.к. этот конструктор должен выполнять идентичные действия как в
+                                        // таком же конструкторе Human, вызываем здесь конструктор родительского класса
         System.out.println("Конструктор mtsTech(String, String)");
     }
 
+    @Override
+    public String toString() {
+        return "mtsTech{" +
+                "jobSpecialization='" + jobSpecialization + '\'' +
+                ", driveLevel=" + driveLevel +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", classId=" + classId +
+                '}';
+    }
 }
