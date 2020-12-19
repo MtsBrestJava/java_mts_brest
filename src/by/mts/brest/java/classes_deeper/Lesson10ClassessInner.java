ccf89e6770bc21f99ca2368dcb0d68fadebdf3a0
sergeblr2 <71922182+sergeblr2@users.noreply.github.com>
2020-12-19 12:24:29 +0300

    Classess Inner 2

--- src/by/mts/brest/java/classes_deeper/Lesson10ClassessInner.java
+++ src/by/mts/brest/java/classes_deeper/Lesson10ClassessInner.java
@@ -7,6 +7,12 @@ public class Lesson10ClassessInner{
         Person tom = new Person("Tom", "qwerty");
         tom.displayPerson();
         tom.account.displayAccount();
+
+        Person somePerson = new Person("Mike", "222");
+        // Person.Account someAccount = new Person.Account("123");
+        Person.Account someAccount = somePerson.new Account("333");
+        System.out.println("somePerson = " + somePerson);
+        System.out.println("someAccount = " + someAccount);
     }
 }
 
@@ -18,6 +24,7 @@ class Person {
     Person(String name, String password){
         this.name = name;
         account = new Account(password);
+        //account.password = "324";
     }
     public void displayPerson(){
         System.out.printf("Person \t Name: %s \t Password: %s \n", name, account.password);
@@ -27,10 +34,26 @@ class Person {
         private String password;
 
         Account(String pass){
+            // Person.this.name = "SomeName";
             this.password = pass;
         }
         void displayAccount(){
             System.out.printf("Account Login: %s \t Password: %s \n", Person.this.name, password);
         }
+
+        @Override
+        public String toString() {
+            return "Account{" +
+                    "password='" + password + '\'' +
+                    '}';
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", account=" + account +
+                '}';
     }
 }
